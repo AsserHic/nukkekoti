b7032eab92cb274516ee5fcad7437fb35f3eb3d7
Marko Laakso <marko.laakso@valuemotive.com>
2020-02-23 14:42:03 +0200

    Simplified led strip logic

--- NukkekotiArduino/NukkekotiArduino.ino
+++ NukkekotiArduino/NukkekotiArduino.ino
@@ -3,21 +3,38 @@
  */
 
 #include <Arduino.h>
+#include <FastLED.h>
 
-#include "LedNauha.h"
+FASTLED_USING_NAMESPACE
+
+// Rakenteelliset parametrit
+#define LEDIEN_MAARA    30
+#define LEDIEN_KIRKKAUS 64
+#define LEDIEN_TYYPPI   WS2811
+#define LEDIEN_VARIT    GRB
 
 // Arduinon nastat:
-const int LED_NAUHAN_OHJAUS = 4;
-const int VARINA_MOOTTORI   = 5;
+#define LED_NAUHAN_OHJAUS 4
+#define VARINA_MOOTTORI   5
+
+CRGB leds[LEDIEN_MAARA];
+uint8_t gHue = 0;
 
 /*
  Alustusrutiini, joka ajetaan vain kerran.
  */
 void setup() {
+  delay(3000); // Kaynnistysviive
+  FastLED.addLeds<LEDIEN_TYYPPI, LED_NAUHAN_OHJAUS, LEDIEN_VARIT>(leds, LEDIEN_MAARA).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(LEDIEN_KIRKKAUS);
 }
 
 /*
  Ohjelman toiminnasta huolehtiva paattymaton silmukka.
  */
 void loop() {
+  fill_rainbow(leds, LEDIEN_MAARA, gHue, 7);
+  FastLED.show();
+  FastLED.delay(100);
+  EVERY_N_MILLISECONDS(20) { gHue++; }
 }
